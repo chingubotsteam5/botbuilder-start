ac97855937ae1c6ce38e92358d3147ac541a6572
spences10 <spences10apps@gmail.com>
2017-06-23 20:39:32 +0100

    add help dialog

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const builder = require('botbuilder')
 // Setup Restify Server
 const server = restify.createServer()
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log('%s listening to %s', server.name, server.url)
+  console.log(`${server.name} listening to ${server.url}`)
 })
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -18,5 +18,19 @@ server.post('/api/messages', connector.listen())
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 const bot = new builder.UniversalBot(connector, session => {
-  session.send('You said: %s', session.message.text)
+  session.send(`You said: ${session.message.text}`)
 })
+
+bot
+  .dialog('help', (session, args, next) => {
+    // Send message to the user and end this dialog
+    session.endDialog('This is a simple bot that collects a name and age.')
+  })
+  .triggerAction({
+    matches: /^help$/,
+    onSelectAction: (session, args, next) => {
+      // Add the help dialog to the dialog stack
+      // (override the default behavior of replacing the stack)
+      session.beginDialog(args.action, args)
+    }
+  })
