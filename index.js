e899267a68fd1c7d538c0aa0bc7a19fdcb6c819f
spences10 <spences10apps@gmail.com>
2017-07-02 08:41:35 +0100

    back to simple bot

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const restify = require('restify')
 const builder = require('botbuilder')
-
-// Setup Restify Server
-const server = restify.createServer()
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log(`${server.name} listening to ${server.url}`)
-})
+const restify = require('restify')
 
 // Create chat connector for communicating with the Bot Framework Service
 const connector = new builder.ChatConnector({
@@ -13,59 +7,23 @@ const connector = new builder.ChatConnector({
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 })
 
-// Listen for messages from users
-server.post('/api/messages', connector.listen())
+// create the bot
+const bot = new builder.UniversalBot(connector)
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-const bot = new builder.UniversalBot(connector, (session) => {
-  session.send(`You said: ${session.message.text}`)
+// add dialog
+bot.dialog('/', [
+  (session) => {
+    builder.Prompts.text(session, 'Please enter your name')
+  },
+  (session, result) => {
+    session.send('Hello, ' + result.response)
+  }
+])
+
+// create the server
+const server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`${server.name} listening to ${server.url}`)
 })
-
-// https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-global-handlers
-// Trigger a help dialog
-bot
-  .dialog('help', (session, args, next) => {
-    // Send message to the user and end this dialog
-    session.endDialog('This is a simple bot that collects a name and age.')
-  })
-  .triggerAction({
-    matches: /^help$/,
-    onSelectAction: (session, args, next) => {
-      // Add the help dialog to the dialog stack
-      // (override the default behavior of replacing the stack)
-      session.beginDialog(args.action, args)
-    }
-  })
-
-bot
-  .dialog('hello', (session, action, args) => {
-    session.endDialog("Hello I'm a parrot")
-  })
-  .triggerAction({
-    matches: /^hello$/,
-    onSelectAction: (session, action, args) => {
-      session.beginDialog(args.action, args)
-    }
-  })
-
-bot
-  .dialog('listBuilderDialog', function(session) {
-    if (!session.dialogData.list) {
-      // Start a new list
-      session.dialogData.list = []
-      session.send(
-        "Each message will added as a new item to the list.\nSay 'end list' when finished or 'cancel' to discard the list.\n"
-      )
-    } else if (/end.*list/i.test(session.message.text)) {
-      // Return current list
-      session.endDialogWithResult({ response: session.dialogData.list })
-    } else {
-      // Add item to list and save() change to dialogData
-      session.dialogData.list.push(session.message.text)
-      session.save()
-    }
-  })
-  .cancelAction('cancelList', 'List canceled', {
-    matches: /^cancel/i,
-    confirmPrompt: 'Are you sure?'
-  })
+// listen on
+server.post('/api/messages', connector.listen())
